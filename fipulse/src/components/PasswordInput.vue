f7a2d9ec1c804e44b21d6311d8cac5584117d92e
LaraQuargnal <116004169+LaraQuargnal@users.noreply.github.com>
2024-03-06 21:44:42 +0100

    added icon eye for password

--- fipulse/src/components/PasswordInput.vue
+++ fipulse/src/components/PasswordInput.vue
@@ -6,19 +6,9 @@
       :id="inputId"
       :placeholder="placeholder"
     />
-    <button
-      type="button"
-      @click="togglePasswordVisibility"
-      style="font-size: 24px; padding: 10px"
-    >
-      <!-- TO DO: add eye icon -->
-
-      <i v-show="passwordVisible" class="fa-eye" style="font-size: 24px"></i>
-      <i
-        v-show="!passwordVisible"
-        class="fa-eye-slash"
-        style="font-size: 24px"
-      ></i>
+    <button type="button" @click="togglePasswordVisibility" class="eye-button">
+      <fa icon="fa-eye" v-show="passwordVisible" class="eye-icon"></fa>
+      <fa icon="fa-eye-slash" v-show="!passwordVisible" class="eye-icon"></fa>
     </button>
   </div>
 </template>
@@ -48,18 +38,3 @@ export default {
   },
 };
 </script>
-  
-  <style scoped>
-.password-input {
-  position: relative;
-}
-
-.password-input button {
-  position: absolute;
-  top: 50%;
-  right: 10px;
-  transform: translateY(-50%);
-  cursor: pointer;
-}
-</style>
-  
