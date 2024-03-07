a617a7994173fd3e0712a368b6c81d3fbbd92dc6
LaraQuargnal <116004169+LaraQuargnal@users.noreply.github.com>
2024-03-07 22:11:37 +0100

    added navbar

--- fipulse/src/App.vue
+++ fipulse/src/App.vue
@@ -1,10 +1,48 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/signup">Signup</router-link>
+    <nav class="navbar bg-body-tertiary" style="margin-bottom: 20px">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="@/assets/logoPrivremeni.png"
+            alt="Logo"
+            height="35"
+            class="d-inline-block align-text-top"
+          />
+        </a>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/signup">Signup</router-link> |
+        <div class="dropdown">
+          <button
+            class="btn btn-link dropdown-toggle custom-dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Language
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <router-link class="dropdown-item" to="">EN</router-link>
+            <router-link class="dropdown-item" to="">HR</router-link>
+          </div>
+        </div>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <router-view />
   </nav>
-  <router-view />
 </template>
 
 <style lang="scss">
@@ -17,7 +55,7 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 20px;
 
   a {
     font-weight: bold;
@@ -27,5 +65,13 @@ nav {
       color: #42b983;
     }
   }
+}
+
+.custom-dropdown-toggle {
+  background-color: transparent !important;
+  font-family: Avenir, Helvetica, Arial, sans-serif !important;
+  border: none !important;
+  color: #2c3e50 !important;
+  font-weight: bold !important;
 }
 </style>
--- fipulse/src/main.js
+++ fipulse/src/main.js
@@ -4,9 +4,14 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'vue-flag-icon'
+
 
 library.add(fas);
 
-createApp(App).use(router)
+createApp(App)
+.use(router)
 .component('fa', FontAwesomeIcon)
 .mount('#app')
